894c79f71040f69644cb473a64fe70141b895b9b
Mehmet-Erkmen <mehmet.erkmen@elev.ga.ntig.se>
2025-03-20 13:00:27 +0100

    Add files via upload
    Forstätter med spelet

--- Fly_från_fängelset.py
+++ Fly_från_fängelset.py
@@ -1,14 +1,17 @@
-#MODULES
+### MODULES ###
 import random, time, os, sys
 
-#VARIABLES
+### VARIABLES ###
 Prisoner_hp = 100
 Prisoner_hitchance = random.randint(7,10)
 prison_break = True
 current_room = "room_1"
 
-#FUNCTIONS
+print (f"Vi är i början av spelet: {current_room}")
 
+### FUNCTIONS ###
+
+#För att skriva sakta bokstav för bokstav
 def print_slow(text):
     for char in text:
         sys.stdout.write(char)
@@ -21,6 +24,7 @@ def clear_terminal():
         os.system('cls')
 
 def room_1(current_room):
+    print (f"Vi är i room_1: {current_room}")
     print_slow("Du är i en cell, som är tom")
     print_slow("1. Söka rummet")
     print_slow("2. Försök öppna dörren")
@@ -31,28 +35,39 @@ def room_1(current_room):
         print_slow("Nu söks rummet...")
         time.sleep(2)
         print_slow("Du har hittat en kniv och skruvmejsel"
-        "\n du har en ventilationsgaller bredvid dig som "
-        "\n går till fängelsets ventilation, vill du öppna den med skruvmejsel "
-        "\n du har?")
+        "\ndu har en ventilationsgaller bredvid dig som "
+        "\ngår till fängelsets ventilation, vill du öppna den med skruvmejsel "
+        "\ndu har?")
     val2 = input("Ja/Nej? ").lower
-    if val2 == "ja":
+    if (val2 == "ja"):
         current_room = "room_2"
     return current_room
 
 def room_2(current_room):
+    print (f"Vi är i room_2: {current_room}")
     clear_terminal()
     print_slow("Nu är du i ventilation")
+    print_slow("Du har två väg, höger eller vänster")
+    print_slow("1. Höger"
+    "\n2. Vänster")
+    val3 = input("Svar (1/2)")
+    # if val3 == "1":
 
+    return current_room
 
     
 
-#test_function
+# def room_2_h():
+#     print_slow()
 
 #Början av spelet
 while (prison_break == True):
     if (current_room == "room_1"):
+        #Anropar funktionen room_1
         current_room = room_1(current_room)
+        print (f"Vi är i spelloopen: {current_room}")
     elif (current_room == "room_2"):
+        #Anropar funktionen room_2
         current_room = room_2(current_room)
     else: 
         exit()
