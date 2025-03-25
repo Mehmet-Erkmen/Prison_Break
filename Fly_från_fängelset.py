8e4cf92cffd54a55581ad5e5cd88537588453727
Mehmet-Erkmen <mehmet.erkmen@elev.ga.ntig.se>
2025-03-25 10:33:26 +0100

    Add files via upload

--- Fly_från_fängelset.py
+++ Fly_från_fängelset.py
@@ -38,27 +38,32 @@ def room_1(current_room):
         "\ndu har en ventilationsgaller bredvid dig som "
         "\ngår till fängelsets ventilation, vill du öppna den med skruvmejsel "
         "\ndu har?")
-    val2 = input("Ja/Nej? ").lower
+    val2 = input("Ja/Nej? ").lower()
     if (val2 == "ja"):
         current_room = "room_2"
-    return current_room
+        return current_room
+    else:
+        print("Du vill stanna kvar här")
+        print("Till slut dör du av svält. Game over.")
+        exit()
 
 def room_2(current_room):
-    print (f"Vi är i room_2: {current_room}")
-    clear_terminal()
-    print_slow("Nu är du i ventilation")
-    print_slow("Du har två väg, höger eller vänster")
-    print_slow("1. Höger"
-    "\n2. Vänster")
-    val3 = input("Svar (1/2)")
-    # if val3 == "1":
-
-    return current_room
+    if current_room == "room_2":
+        print (f"Vi är i room_2: {current_room}")
+        clear_terminal()
+        print_slow("Nu är du i ventilation")
+        print_slow("Du har två väg, höger eller vänster")
+        print_slow("1. Höger"
+        "\n2. Vänster")
+        val3 = input("Svar (1/2)")
+        if val3 == "1":
+            print_slow()
+        return current_room
 
     
 
-# def room_2_h():
-#     print_slow()
+def room_2_h():
+    print_slow()
 
 #Början av spelet
 while (prison_break == True):
