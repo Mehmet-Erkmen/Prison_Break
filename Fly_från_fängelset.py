693fbdb0460653fb4c964150cdf8995e9e2aae89
Mehmet-Erkmen <mehmet.erkmen@elev.ga.ntig.se>
2025-05-04 13:57:19 +0200

    Add files via upload
    Klar med spelet

--- Fly_från_fängelset.py
+++ Fly_från_fängelset.py
@@ -3,11 +3,11 @@ import random, time, os, sys
 
 ### VARIABLES ###
 Prisoner_hp = 100
-Prisoner_hitchance = random.randint(7,10)
 prison_break = True
 current_room = "meny"
-guardhp = 100
+Guardhp = 100
 Samuraihp = 100
+Chefhp = 500
 loop_room_1= True
 loop_room_2 = True
 loop_room_3 = True
@@ -131,7 +131,7 @@ def room_2(current_room):
         return current_room 
 
 #Definitionen till funktionen för tredje rummet(Mini Boss)
-def room_3(current_room):
+def room_3(current_room, Prisoner_hp):
     while loop_room_3 == True:
         clear_terminal()
         print_slow("Nu är du i en stor rum, det är olika vapen i vägen, det finns blod på de. ")
@@ -152,66 +152,79 @@ def room_3(current_room):
             print_slow("Två moves, första(1) är AOE damage baserat move, vilket ger damage i ett område för att skada fler mål.")
             print_slow("Andra(2) är att du använder elemten blixten som laddar dina katanas, du får inte lika mycket damage men högre träff chans.")
             movesinput = input("Svar 1/2: ")
-            if weaponinput == "1":
-                characterweapon = "Katana"
-                katchancelightning = random.randint(1,100)
-                katchanceaoe = random.randint(1,100)
-                #Katanas med laddad blixt
-                if movesinput == "1":
-                    if katchancelightning < 25:
-                        katdamage = 0
-                    elif katchancelightning >=25:
-                        katdamage = 10
-                    elif katchancelightning >= 50:
-                        katdamage = 20
-                    elif katchancelightning >= 75:
-                        katdamage = 30
-                    elif katchancelightning == 100:
-                        katdamage = 100
+            katchancelightning = random.randint(1,100)
+            katchanceaoe = random.randint(1,100)
+            #Katanas med laddad blixt
+            if movesinput == "1":
+                if katchancelightning < 25:
+                    katdamage = 0
+                elif katchancelightning >=25:
+                    katdamage = 10
+                elif katchancelightning >= 50:
+                    katdamage = 20
+                elif katchancelightning >= 75:
+                    katdamage = 30
+                elif katchancelightning == 100:
+                    katdamage = 100
                 #Katanas med AOE
-                if movesinput == "2":
-                    if katchanceaoe < 25:
-                        katdamage = 0
-                    elif katchanceaoe >=25:
-                        katdamage = 5
-                    elif katchanceaoe >= 50:
-                        katdamage = 10
-                    elif katchanceaoe >= 75:
-                        katdamage = 15
-                    elif katchanceaoe == 100:
-                        katdamage = 20
-            elif weaponinput == "2":
-                characterweapon = "Big Sledge Hammer"
-                ham_chance_sonic = random.randint(1,100)
-                ham_chance_snurr = random.randint(1,100)
-                if ham_chance_snurr >=25:
-                    hamdamage = 10
-                elif ham_chance_snurr >= 50:
-                    hamdamage = 30
-                elif ham_chance_snurr >= 75:
-                    hamdamage = 55
-                elif ham_chance_snurr == 100:
-                    hamdamage = 70
-                if ham_chance_sonic >=25:
-                    hamdamage = 10
-                elif ham_chance_sonic >= 50:
-                    hamdamage = 30
-                elif ham_chance_sonic >= 75:
-                    hamdamage = 55
-                elif ham_chance_sonic == 100:
-                    hamdamage = 70
+            if movesinput == "2":
+                if katchanceaoe < 25:
+                    katdamage = 0
+                elif katchanceaoe >=25:
+                    katdamage = 5
+                elif katchanceaoe >= 50:
+                    katdamage = 10
+                elif katchanceaoe >= 75:
+                    katdamage = 15
+                elif katchanceaoe == 100:
+                    katdamage = 20
+        elif weaponinput == "2":
+            characterweapon = "Big Sledge Hammer"
+            ham_chance_sonic = random.randint(1,100)
+            ham_chance_snurr = random.randint(1,100)
+            if ham_chance_snurr >=25:
+                hamdamage = 10
+            elif ham_chance_snurr >= 50:
+                hamdamage = 30
+            elif ham_chance_snurr >= 75:
+                hamdamage = 55
+            elif ham_chance_snurr == 100:
+                hamdamage = 70
+            if ham_chance_sonic >=25:
+                hamdamage = 10
+            elif ham_chance_sonic >= 50:
+                hamdamage = 30
+            elif ham_chance_sonic >= 75:
+                hamdamage = 55
+            elif ham_chance_sonic == 100:
+                hamdamage = 70
 
-            elif weaponinput == "Ragnarök":
-                characterweapon = "Mjölnir"
-                mjölnir_damage_bifrost = 999
-                print_slow("Med mjölnir du kan dina motståndare till andra värld")
-            elif weaponinput == "Nuttertools":
-                characterweapon == "MiniGun"
-                minidamage = 999
-                print_slow("Med minigun kan du skjuta och döda inom några sekunder")
+        elif weaponinput == "Ragnarök":
+            characterweapon = "Mjölnir"
+            mjölnir_damage_bifrost = 999
+            print_slow("Med mjölnir du kan dina motståndare till andra värld")
+        elif weaponinput == "Nuttertools":
+            characterweapon == "MiniGun"
+            minidamage = 999
+            print_slow("Med minigun kan du skjuta och döda inom några sekunder")
 
             Samuraihp = Samuraihp - hamdamage or katdamage or mjölnir_damage_bifrost or minidamage
-            
+
+            Samurai_hitchance = random.randint(1,100)
+            if Samurai_hitchance >= 25:
+                samuraidamage = 20
+            elif Samurai_hitchance >= 50:
+                samuraidamage = 40
+            elif Samurai_hitchance >= 75:
+                samuraidamage = 60
+            elif Samurai_hitchance == 100:
+                samuraidamage = 100
+
+            Prisoner_hp = Prisoner_hp - samuraidamage
+        if Prisoner_hp == 0:
+            print_slow("Du är död, du får börja om ett rum tidigare.")
+            current_room = "room_2"
+
         if Samuraihp == 0:
             print_slow("Nu är Samurai är död. ")
             print_slow("I rummet har dökt upp en dörr, vad ska du göra nu? ")
@@ -222,10 +235,11 @@ def room_3(current_room):
                 current_room = "room_4"
             elif val6 == "2":
             
-                return characterweapon, current_room, hamdamage, ham_chance_snurr, ham_chance_sonic, katchanceaoe, katchancelightning, katdamage, mjölnir_damage_bifrost, minidamage
+                return characterweapon, current_room, ham_chance_snurr, ham_chance_sonic, katchanceaoe, katchancelightning
 
 #Definitionen för fjärde rummet(Pusselrummet) 
-def room_4():
+def room_4(current_room):
+    clear_terminal()
     riddle1_chance = 0
     riddle2_chance = 0
     riddle3_chance = 0
@@ -256,34 +270,124 @@ def room_4():
                         if val7 == "1":
                             print_slow("Du valde att gå igenom dörren")
                             current_room = "room_5"
-                    elif riddleinput3 != "Eko":
+                    elif riddleinput3 != "Eko" and riddle3_chance != 4:
                         riddle3_chance += 1
                         print_slow("Du klara inte tredje gåtan. Testa igen!")
-                elif riddleinput2 != "Ägg":
+                elif riddleinput2 != "Ägg" and riddle2_chance != 4:
                     riddle2_chance += 1
                     print_slow("Du klara inte andra gåtan. Testa igen!")
-            elif riddleinput1 != "Andetag":
+            elif riddleinput1 != "Andetag" and riddle1_chance != 4:
                 riddle1_chance += 1
                 print_slow("Du klara inte första gåtan. Testa igen!")
+    return current_room
 
+def room_5(characterweapon, ham_chance_snurr, ham_chance_sonic, katchanceaoe, katchancelightning, Prisoner_hp):
+    clear_terminal()
+    print_slow("I rummet så finns det en bord och stol som ryggen är mot dig.")
+    print_slow('Stolen vänder mot dig, vilket sitter "Chefen". Han säger att "Jag kommer stoppa dig och att du kommer att dö.')
+    print_slow('Du kommer inte äns tillbaka till din cell eller byta fängelse. Du kommer att dö."')
+    print_slow("Om du klarar att döda chefen så är du FRI! Lycka till!")
+    while loop_room_5 == True:
+        if characterweapon == "Katana":
+            print_slow("Två moves, första(1) är AOE damage baserat move, vilket ger damage i ett område för att skada fler mål.")
+            print_slow("Andra(2) är att du använder elemten blixten som laddar dina katanas, du får inte lika mycket damage men högre träff chans.")
+            movesinput = input("Svar 1/2: ")
+            if movesinput == "1":
+                if katchancelightning < 25:
+                    katdamage = 0
+                elif katchancelightning >=25:
+                    katdamage = 10
+                elif katchancelightning >= 50:
+                    katdamage = 20
+                elif katchancelightning >= 75:
+                    katdamage = 30
+                elif katchancelightning == 100:
+                    katdamage = 100
+                    #Katanas med AOE
+            if movesinput == "2":
+                if katchanceaoe < 25:
+                    katdamage = 0
+                elif katchanceaoe >=25:
+                    katdamage = 5
+                elif katchanceaoe >= 50:
+                    katdamage = 10
+                elif katchanceaoe >= 75:
+                    katdamage = 15
+                elif katchanceaoe == 100:
+                    katdamage = 20
+                if characterweapon == "Big Sledge Hammer":
+                    if ham_chance_snurr >=25:
+                        hamdamage = 10
+                    elif ham_chance_snurr >= 50:
+                        hamdamage = 30
+                    elif ham_chance_snurr >= 75:
+                        hamdamage = 55
+                    elif ham_chance_snurr == 100:
+                        hamdamage = 70
+                    if ham_chance_sonic >=25:
+                        hamdamage = 10
+                    elif ham_chance_sonic >= 50:
+                        hamdamage = 30
+                    elif ham_chance_sonic >= 75:
+                        hamdamage = 55
+                    elif ham_chance_sonic == 100:
+                        hamdamage = 70
 
-
+                if characterweapon == "Mjölnir":
+                    mjölnir_damage_bifrost = 999
+                    print_slow("Med mjölnir du kan dina motståndare till andra värld")
+                if characterweapon == "MiniGun":
+                    minidamage = 999
+                    print_slow("Med minigun kan du skjuta och döda inom några sekunder")
+        Chefhp = Chefhp - mjölnir_damage_bifrost or katdamage or hamdamage or minidamage
+        if Chefhp == 0:
+            print_slow("Du har dödat chefen, nu har ett dörr har dökt upp. Den är en magisk dörr som när du går igenom den ska teleportera dig till där ingen kan hitta dig.")
+            print_slow("Vad vill du göra?")
+            print_slow("(1) Gå igenom dörren")
+            print_slow("(2) Stå kvar")
+            flyktval = input("Svar: ")
+            if flyktval == "1":
+                print_slow("Du har valt att gå igenom dörren vilket är en smart val. Nu lever du med glädje. Nu lever du i Sydamerika")
+                print_slow("Nu är du en av de rikaste personer just nu.")
+                print_slow("Hejdå...")
+                current_room = "finish"
+            elif flyktval == "2":
+                print_slow("Du har valt att stanna kvar och vakter kom. Du ")
+        chef_hitchance = random.randint(1,100)
+        if chef_hitchance >= 25:
+            chefdamage = 15
+        elif chef_hitchance >= 50:
+            chefdamage = 45
+        elif chef_hitchance >= 75:
+            chefdamage = 80
+        elif chef_hitchance == 100:
+            chefdamage = 100
+        Prisoner_hp = Prisoner_hp - chefdamage
+        if Prisoner_hp == 0:
+            print_slow("Du är död, du får börja om ett rum tidigare.")
+            current_room = "room_4"
+        return current_room
 #Början av spelet
 while (prison_break == True):
-    room_4()
     clear_terminal()
     #menyn för spelet i loopen
     if current_room == "meny":
         current_room = meny(current_room)
     #Anropar funktionen room_1
-    if (current_room == "room_1"):
+    elif (current_room == "room_1"):
         current_room = room_1(current_room)
     #Anropar funktionen room_2
     elif (current_room == "room_2"):
         current_room = room_2(current_room)
     #Anropar funktionen room_3
     elif (current_room == "room_3"):
-        current_room = room_3(current_room)
+        current_room = room_3(current_room, Prisoner_hp)
+    #Anropar funktionen room_4
+    elif (current_room == "room_4"):
+        current_room = room_4(current_room)
+    #Anropar funktionen room_5
+    elif (current_room == "room_5"):
+        current_room = room_5(current_room, Prisoner_hp)
     #När spelet slutar
     else: 
         exit()
